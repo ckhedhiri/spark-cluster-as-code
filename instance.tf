09956d4c44826d72c7547e800736adfd9451f45a
ckhedhiri <ckhedhiri@wewyse.com>
2021-10-27 23:36:11 +0200

    add private access key

--- instance.tf
+++ instance.tf
@@ -35,7 +35,7 @@ data "template_file" "jupyter_init" {
 resource "aws_instance" "spark_master" {
   ami           = "${data.aws_ami.spark_ami.id}"
   instance_type = "t2.micro"
-  key_name      = "${aws_key_pair.mykeypair.key_name}"
+  key_name      = "${aws_key_pair.ec2_key.key_name}"
   vpc_security_group_ids = ["${aws_security_group.spark-sg.id}"]
   subnet_id = "${module.vpc.private_subnets[0]}"
   user_data = "${data.template_file.spark_master_init.rendered}"
@@ -47,7 +47,7 @@ resource "aws_instance" "spark_master" {
 resource "aws_instance" "spark_slave" {
   ami           = "${data.aws_ami.spark_ami.id}"
   instance_type = "t2.micro"
-  key_name      = "${aws_key_pair.mykeypair.key_name}"
+  key_name      = "${aws_key_pair.ec2_key.key_name}"
   vpc_security_group_ids = ["${aws_security_group.spark-sg.id}"]
   subnet_id = "${module.vpc.private_subnets[0]}"
   user_data = "${data.template_file.spark_slave_init.rendered}"
@@ -60,7 +60,7 @@ resource "aws_instance" "spark_slave" {
 resource "aws_instance" "jupyter" {
   ami           = "${data.aws_ami.spark_ami.id}"
   instance_type = ""
-  key_name      = "${aws_key_pair.mykeypair.key_name}"
+  key_name      = "${aws_key_pair.ec2_key.key_name}"
   vpc_security_group_ids = ["${aws_security_group.jupyter-sg.id}"]
   subnet_id = "${module.vpc.public_subnets[0]}"
   user_data = "${data.template_file.jupyter_init.rendered}"
