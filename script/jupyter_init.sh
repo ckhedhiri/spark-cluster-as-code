afef5d324305b8d3ecb1832d6f7c5a244e63f145
ckhedhiri <ckhedhiri@wewyse.com>
2021-11-14 22:51:57 +0100

    fix jupyter script

--- script/jupyter_init.sh
+++ script/jupyter_init.sh
@@ -19,9 +19,9 @@ echo "c.NotebookApp.port = 8888" >> /home/ubuntu/.jupyter/jupyter_notebook_confi
 echo "c.NotebookApp.token = ''" >> 	/home/ubuntu/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.password = u''" >> /home/ubuntu/.jupyter/jupyter_notebook_config.py
 
-echo "export PYSPARK_DRIVER_PYTHON='jupyter'" >> /etc/profile.d/jupyter_env.sh
-echo "export PYSPARK_DRIVER_PYTHON_OPTS='notebook'" >> /etc/profile.d/jupyter_env.sh
-echo "export PYTHONPATH=$SPARK_HOME/python/:$PYTHONPATH" >> /etc/profile.d/jupyter_env.sh
-echo "export PYTHONPATH=$SPARK_HOME/python/lib/py4j-0.10.9.2-src.zip:$PYTHONPATH" >> /etc/profile.d/jupyter_env.sh
+sudo echo "export PYSPARK_DRIVER_PYTHON='jupyter'" >> /etc/profile.d/jupyter_env.sh
+sudo echo "export PYSPARK_DRIVER_PYTHON_OPTS='notebook'" >> /etc/profile.d/jupyter_env.sh
+sudo echo "export PYTHONPATH=$SPARK_HOME/python/:$PYTHONPATH" >> /etc/profile.d/jupyter_env.sh
+sudo echo "export PYTHONPATH=$SPARK_HOME/python/lib/py4j-0.10.9.2-src.zip:$PYTHONPATH" >> /etc/profile.d/jupyter_env.sh
 
 jupyter notebook &
